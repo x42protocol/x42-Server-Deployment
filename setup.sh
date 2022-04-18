a7fb2bbb8328e48670608416873a5004c491d544
Panayiotis Savva <psavva@gmail.com>
2022-04-18 21:05:57 +0300

    update playbook execution

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ apt install software-properties-common -y
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
 
-ansible-playbook -i ansible/x42docker.yaml
-ansible-playbook -i ansible/x42xserver.yaml
+ansible-playbook ansible/x42docker.yaml
+ansible-playbook ansible/x42xserver.yaml
